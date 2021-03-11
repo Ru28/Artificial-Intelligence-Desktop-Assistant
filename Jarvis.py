c776af835a020c9ef66cbcaa9216a57036db7d98
Rupesh Virani <54779977+Ru28@users.noreply.github.com>
2021-03-11 22:10:58 +0530

    version1

--- Jarvis.py
+++ Jarvis.py
@@ -53,7 +53,7 @@ def sendEmail(to,content):
     server= smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
-    server.login('abc@gmail.com','password-here')
+    server.login('youremail@gmail.com','your-password')
     server.sendmail('youremail@gmail.com',to,content)
     server.close()
 
